b2de7e492ee9b82892b6abc0258174b6e7fee509
Junjie Yang <yangjunjie0320@gmail.com>
2024-08-12 15:13:55 -0700

    update

--- phi-in-supercell.py
+++ phi-in-supercell.py
@@ -26,33 +26,17 @@ from pyscf.pbc.tools.k2gamma import translation_vectors_for_kmesh
 vr = rpts = translation_vectors_for_kmesh(cell, kmesh=kmesh, wrap_around=False)
 nr = len(vr)
 
-scell = pyscf.pbc.tools.pbc.super_cell(cell.copy(deep=True), kmesh, wrap_around=False)
-
-gmesh = numpy.asarray([10] * 3)
+gmesh = numpy.asarray([5] * 3)
 ng = numpy.prod(gmesh)
-
 coord0 = cell.gen_uniform_grids(gmesh, wrap_around=False)
-coord1 = coord0[None, :] + vr[:, None]
-coord1 = coord1.reshape(nr * ng, 3)
 
-phi0 = scell.pbc_eval_gto('GTOval', coord0)
-phi0 = phi0.reshape(ng, nr, nao)
+phik = cell.pbc_eval_gto('GTOval', coord0, kpts=vk)
+phik = numpy.asarray(phik)
+assert phik.shape == (nk, ng, nao)
 
-phi = scell.pbc_eval_gto('GTOval', coord1)
-zeta1 = numpy.einsum("Im,Jm,In,Jn->IJ", phi, phi, phi, phi, optimize=True)
-zeta1 = zeta1.reshape(nr, ng, nr, ng)
-rho1 = numpy.einsum("gm,gn->gmn", phi, phi, optimize=True)
-rho1 = rho1.reshape(nr, ng, nr, nao, nr, nao)
-print(f"zeta1: {zeta1.shape}")
-print(f"rho1: {rho1.shape}")
-
-phi = phi.reshape(nr, ng, nr, nao)
-rho2 = numpy.einsum("rgsm,rgtn->rgsmtn", phi, phi, optimize=True)
-assert rho2.shape == (nr, ng, nr, nao, nr, nao)
-
-rho3 = numpy.einsum("grm,gsn->grmsn", phi0, phi0, optimize=True)
-assert rho3.shape == (ng, nr, nao, nr, nao)
-rhs = numpy.einsum("grm,grm,hrm,hrm->gh", phi0, phi0, phi0, phi0, optimize=True)
+rhs = numpy.einsum("kgm,khm->gh", phik.conj(), phik, optimize=True) / cell.vol * 2
+rhs = rhs.conj() * rhs
+rhs = rhs.real
 
 from pyscf.lib.scipy_helper import pivoted_cholesky
 chol, perm, rank = pivoted_cholesky(rhs)
@@ -60,8 +44,4 @@ perm = perm[:rank]
 zeta = rhs[:, perm] @ numpy.linalg.pinv(rhs[perm][:, perm])
 print(perm)
 
-rho4 = numpy.einsum("gI,Irm,Isn->grmsn", zeta, phi0[perm], phi0[perm], optimize=True)
-assert rho4.shape == (ng, nr, nao, nr, nao)
-err = abs(rho3 - rho4).max()
-assert err < 1e-10, err
-print("All tests passed")
+for k1k2 in range
